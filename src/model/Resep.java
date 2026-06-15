16698494f41a5694ae24ad308681c552bd6368c2
Tahoo <Tahoo@Tahoo>
2026-06-15 12:34:42 +0700

    first commit

--- src/model/Resep.java
+++ src/model/Resep.java
@@ -3,11 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
+import java.sql.Date;
 /**
  *
  * @author VanZ
  */
 public class Resep {
+     private int idResep;
+    private int idPemeriksaan;
+    private Date tanggalResep;
+
+    // Konstruktor Default
+    public Resep() {
+    }
+
+    // Konstruktor Custom
+    public Resep(int idPemeriksaan, Date tanggalResep) {
+        this.idPemeriksaan = idPemeriksaan;
+        this.tanggalResep = tanggalResep;
+    }
+
+    // Getter dan Setter
+    public int getIdResep() {
+        return idResep;
+    }
+
+    public void setIdResep(int idResep) {
+        this.idResep = idResep;
+    }
+
+    public int getIdPemeriksaan() {
+        return idPemeriksaan;
+    }
+
+    public void setIdPemeriksaan(int idPemeriksaan) {
+        this.idPemeriksaan = idPemeriksaan;
+    }
+
+    public Date getTanggalResep() {
+        return tanggalResep;
+    }
+
+    public void setTanggalResep(Date tanggalResep) {
+        this.tanggalResep = tanggalResep;
+    }
     
 }
