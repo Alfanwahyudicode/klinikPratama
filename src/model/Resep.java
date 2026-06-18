95cb35ec357c46e684846e348ce47c5589b55b9f
Tahoo <Tahoo@Tahoo>
2026-06-19 05:18:10 +0700

    first commit

--- src/model/Resep.java
+++ src/model/Resep.java
@@ -4,40 +4,21 @@
  */
 package model;
 import java.sql.Date;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+
 /**
  *
  * @author VanZ
  */
 public class Resep {
-       private int    idResep;
-    private int    idPemeriksaan;
-    private Date   tanggalResep;
+      private int  idResep;
+    private int  idPemeriksaan;
+    private Date tglResep;
  
-    private String namaPasien;
-    private String namaDokter;
-    private String noRm;
-    private String diagnosa;
- 
-    private List<ResepDetail> listDetail = new ArrayList<>();
-  
     public Resep() {}
  
-    public Resep(int idPemeriksaan, Date tanggalResep) {
+    public Resep(int idPemeriksaan, Date tglResep) {
         this.idPemeriksaan = idPemeriksaan;
-        this.tanggalResep  = tanggalResep;
-    }
-
-    public BigDecimal getTotalObat() {
-        BigDecimal total = BigDecimal.ZERO;
-        for (ResepDetail rd : listDetail) {
-            if (rd.getSubtotal() != null) {
-                total = total.add(rd.getSubtotal());
-            }
-        }
-        return total;
+        this.tglResep      = tglResep;
     }
  
     public int getIdResep()                  { return idResep; }
@@ -46,29 +27,12 @@ public class Resep {
     public int getIdPemeriksaan()                    { return idPemeriksaan; }
     public void setIdPemeriksaan(int idPemeriksaan)  { this.idPemeriksaan = idPemeriksaan; }
  
-    public Date getTanggalResep()                { return tanggalResep; }
-    public void setTanggalResep(Date tanggalResep) { this.tanggalResep = tanggalResep; }
- 
-    public String getNamaPasien()                { return namaPasien; }
-    public void setNamaPasien(String namaPasien) { this.namaPasien = namaPasien; }
- 
-    public String getNamaDokter()                { return namaDokter; }
-    public void setNamaDokter(String namaDokter) { this.namaDokter = namaDokter; }
- 
-    public String getNoRm()              { return noRm; }
-    public void setNoRm(String noRm)     { this.noRm = noRm; }
- 
-    public String getDiagnosa()                  { return diagnosa; }
-    public void setDiagnosa(String diagnosa)     { this.diagnosa = diagnosa; }
- 
-    public List<ResepDetail> getListDetail()                         { return listDetail; }
-    public void setListDetail(List<ResepDetail> listDetail)          { this.listDetail = listDetail; }
+    public Date getTglResep()                { return tglResep; }
+    public void setTglResep(Date tglResep)   { this.tglResep = tglResep; }
  
     @Override
     public String toString() {
-        return "Resep #" + idResep + " | Pemeriksaan #" + idPemeriksaan
-               + " | " + (namaPasien != null ? namaPasien : "") 
-               + " | " + tanggalResep;
+        return "Resep #" + idResep + " | Pemeriksaan #" + idPemeriksaan + " | " + tglResep;
     }
 
 }
